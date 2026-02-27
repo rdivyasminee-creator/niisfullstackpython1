31b4d48b3f3040ce9cbc0be3ee4ad0dd33f104f5
divyaa <rdivyasminee@example.com>
2026-02-27 21:31:24 +0530

    function

---
+++ function/intrst2.py
@@ -0,0 +1,10 @@
+def sical(p,r,t):
+	si=p*r*t
+	print("simple interest=",si)
+print("enter principal")
+p=float(input())
+print("enter rate")
+r=float(input())
+print("enter time")
+t=float(input())
+sical(p,r,t)
